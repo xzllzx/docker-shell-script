59ba4e3b84730fbe6f55beeab62cc3245a778156
xzllzx <73070369+xzllzx@users.noreply.github.com>
2023-04-24 17:12:20 +0800

    Fix Bash script

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -12,10 +12,11 @@ declare -A version_and_sha=(
     ["3.0"]="da634bf2c1e6bbfb36cc3172966ede6c9a8c7dc8",
 )
 
-mkdir temp_repo
+mkdir -p temp_repo
 cd ./temp_repo
 # Clone the specific version of the repository using the personal access token
-git clone https://${GIT_ACCESS_TOKEN}@{GIT_REPOSITORY} .
+# printf "\nCloning https://%s:%s@%s\n" "${GIT_USERNAME}" "${GIT_ACCESS_TOKEN}" "${GIT_REPOSITORY}"
+git clone "https://${GIT_USERNAME}:${GIT_ACCESS_TOKEN}@${GIT_REPOSITORY}" .
 
 # Iterate through the dictionary and build, tag, and push the Docker images
 for version in "${!version_and_sha[@]}"; do
@@ -24,13 +25,15 @@ for version in "${!version_and_sha[@]}"; do
     git checkout $sha
 
     # Build the Docker image with the specific version number
-    docker build -t {IMAGE_NAME}:$version .
+    docker build -t ${IMAGE_NAME}:$version .
 
     # Tag the Docker image with your Docker Hub username and repository
-    docker tag {IMAGE_NAME}:$version ${DOCKER_USERNAME}/{IMAGE_NAME}:$version
+    docker tag ${IMAGE_NAME}:$version ${DOCKER_USERNAME}/${IMAGE_NAME}:$version
 
     # Push the Docker image to Docker Hub
-    docker push ${DOCKER_USERNAME}/{IMAGE_NAME}:$version
+    docker push ${DOCKER_USERNAME}/${IMAGE_NAME}:$version
+    echo "Version $version successfully pushed"
+
 
 done
 
